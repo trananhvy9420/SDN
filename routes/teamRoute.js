3a6ac345d938461c7cdfadfabe0cc58f46a7bd95
trananhvy9420 <trananhvy9420@gmail.com>
2025-06-21 22:06:48 +0700

    Thêm validate full cho teamRouteteamRoute

--- routes/teamRoute.js
+++ routes/teamRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const teamRoute = express.Router();
 const team = require("../models/team");
 const teamController = require("../controllers/team.controller");
-const { query, param, body } = require("express-validator");
+const { query, param, body, validationResult } = require("express-validator");
 const {
   registerRules,
   validate,
@@ -10,53 +10,75 @@ const {
   protectedRoute,
   isAdmin,
 } = require("../middlewares/validation.middleware");
-teamRoute
-  .route("/")
-  .get(
-    [
-      query("page")
-        .optional()
-        .isInt({ min: 1 })
-        .withMessage("Page must be a positive number"),
-      query("limit")
-        .optional()
-        .isInt({ min: 1 })
-        .withMessage("Limit must be a positive number"),
-    ],
-    protectedRoute,
-    isAdmin,
-    teamController.findAllTeam
-  );
 
-teamRoute.route("/").post(protectedRoute, isAdmin, teamController.createTeam);
+teamRoute.route("/").get(
+  [
+    query("page")
+      .optional()
+      .isInt({ min: 1 })
+      .withMessage("Page must be a positive number"),
+    query("limit")
+      .optional()
+      .isInt({ min: 1 })
+      .withMessage("Limit must be a positive number"),
+  ],
+
+  protectedRoute,
+  isAdmin,
+  validate,
+  teamController.findAllTeam
+);
+
+teamRoute.route("/").post(
+  [
+    body("teamName")
+      .trim()
+      .notEmpty()
+      .withMessage("Team Name must be required"),
+  ],
+
+  protectedRoute,
+  isAdmin,
+  validate,
+  teamController.createTeam
+);
 teamRoute
   .route("/:id")
   .get(
-    [param("id").notEmpty().withMessage("ID must be required")],
+    [
+      param("id")
+        .notEmpty()
+        .withMessage("ID must be required")
+        .isMongoId()
+        .withMessage("Invalid ID format."),
+    ],
     protectedRoute,
     isAdmin,
+    validate,
     teamController.findByID
   );
-teamRoute
-  .route("/:id")
-  .put(
-    [
-      param("id").notEmpty().withMessage("ID must be required"),
-      body("teamName")
-        .trim()
-        .notEmpty()
-        .withMessage("Team Name must be required"),
-    ],
-    protectedRoute,
-    isAdmin,
-    teamController.updateTeam
-  );
+//update
+teamRoute.route("/:id").put(
+  [
+    param("id").notEmpty().withMessage("ID must be required"),
+    body("teamName")
+      .trim()
+      .notEmpty()
+      .withMessage("Team Name must be required"),
+  ],
+
+  protectedRoute,
+  isAdmin,
+  validate,
+  teamController.updateTeam
+);
 teamRoute
   .route("/delete/:id")
   .delete(
     [param("id").notEmpty().withMessage("ID must be required")],
     protectedRoute,
     isAdmin,
+    validate,
     teamController.deleteTeam
   );
 module.exports = teamRoute;
